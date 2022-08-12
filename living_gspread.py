98e222a8fb78d6393902f1c8846157683e6cf128
mate.mrse <mate.mrse@intis.hr>
2022-08-12 11:54:39 +0200

    initial

---
+++ living_gspread.py
@@ -0,0 +1,40 @@
+from datetime import date
+from time import perf_counter
+import requests
+from bs4 import BeautifulSoup
+import gspread
+
+import email_sender
+
+
+def main():
+    start = perf_counter()
+    base_url_cost = "https://www.numbeo.com/cost-of-living/in/"
+    gradovi = ["Split", "Zagreb", "Zadar", "Dubrovnik", "Rijeka", "Osijek", "Solin-Croatia", "Makarska-Croatia",
+                "Trogir-Croatia", "Sibenik-Croatia", "Kaštela-Croatia", "Omis-Croatia"]
+    currency = "EUR"
+
+    gc = gspread.service_account(filename='creds-google.json')
+
+    for count, grad in enumerate(gradovi):
+
+        sh = gc.open('cost_of_living')
+        worksheet = sh.worksheet(grad)
+        response = requests.get(base_url_cost + grad + f"?displayCurrency={currency}")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        vrijednosti = soup.find_all("td", class_="priceValue")
+        cost_of_living = {}
+
+        for vrijednost in vrijednosti:
+            cost_of_living[vrijednost.findParent('tr').findNext('td').get_text()] = vrijednost.get_text().split()[0]
+
+        worksheet.append_row([f"{date.today()}"]+list(cost_of_living.values()))
+
+    print(f"Vrijeme izvršavanja: {perf_counter() - start} sekundi.")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        email_sender.send_email(f'Living gspread {date.today()}', 'senfsend@outlook.com', 'mate.mrse@gmail.com', f"{e}")
